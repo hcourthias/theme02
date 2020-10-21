1447fff2054fec2a07af6b2a5313b12f38d975b2
Hugo <hugo.courthias@epitech.eu>
2020-10-21 17:50:51 +0200

    Added city to data

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -14,6 +14,7 @@ import moment from 'moment'
   export default {
     data() {
       return {
+        city: null,
         temperature: null,
         date: moment().locale("fr").format("Do MMMM YYYY, HH:mm"),       
       }
