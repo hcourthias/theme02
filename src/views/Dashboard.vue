0495e8272849f2bce83c1680a801da269c5aae06
Hugo <hugo.courthias@appstud.com>
2020-10-22 17:38:22 +0200

    Added debug button on dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8 text-center">
+          <base-button type="primary" class="my-4" v-on:click="testAPI">TEST API</base-button>
         </base-header>
     </div>
 </template>
 <script>
+  import {getUser} from "../api/api"
 
   export default {
     components: {
@@ -16,7 +18,10 @@
       };
     },
     methods: {
-      
+      testAPI() {
+        console.log("testAPI")
+        getUser(1100).then((res) => console.log(res)).catch(() => console.log("error API"))
+      }
     },
     mounted() {
     }
