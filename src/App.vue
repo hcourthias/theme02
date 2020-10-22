45b3c9239f210c63b527b38ea4f7fc79a3f01487
basile <basile.rolland31@gmail.com>
2020-10-22 14:32:53 +0200

    add promise

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
     <div id="container">
         <div class="sign">
             <p>Name</p>
-            <input type="text" name="name" v-model="name">
+            <input type="text" name="namein" v-model="name">
             <p>eMail</p>
-            <input type="text" name="email" v-model="email">
+            <input type="text" name="emailin" v-model="email">
             <button v-on:click="getUser">Sign In</button>
         </div>
         <div class="sign">
             <p>Name</p>
             <input type="text" name="nameOut" v-model="name">
             <p>eMail</p>
-            <input type="text" name="email" v-model="email">
+            <input type="text" name="emailout" v-model="email">
             <button v-on:click="createUser">Sign Out</button>
         </div>
     </div>
@@ -28,18 +28,20 @@
         },
         methods: {
           createUser() {
-            API.createUser(this.email,this.name)
-          }
-          ,
+            API.createUser(this.email,this.name).then(response => console.log(response)).catch(error=> alert(error  ))
+          },
           updateUser(){
-            
+            // API.updateUser(id,name)
           },
           getUser(){
             console.log(this.email+" fzefezf "+ this.name)
-            API.getUserByEmailAndUsername(this.email,this.name)
+            API.getUserByEmailAndUsername(this.email,this.name).then(resp => console.log(resp.data.data.id))
+            // if (retour!="erreur"){
+              
+            // }
           },
           deleteUser() {
-
+            API.deleteUser()
           },
         }
     }
