9d8dad44e473c6e016cc76ba6fdfc42c128eed96
basile <basile.rolland31@gmail.com>
2020-10-22 12:57:01 +0200

    start of work on User login

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,65 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <h1>Où ?</h1>
-      <select v-model="selected" class="select">
-        <option
-          v-for="option in options"
-          v-bind:value="option.value"
-          v-bind:key="option.key"
-        >
-          {{ option.value }}
-        </option>
-      </select>
-      <div>
-        <router-link class="button" :to="{name: 'currentWeather', params: { city: selected }}">Search</router-link>
-      </div>
+    <div id="container">
+        <div class="sign">
+            <p>Name</p>
+            <input type="text" name="name" v-model="name">
+            <p>eMail</p>
+            <input type="text" name="email" v-model="email">
+            <button v-on:click="getUser">Sign In</button>
+        </div>
+        <div class="sign">
+            <p>Name</p>
+            <input type="text" name="nameOut" v-model="name">
+            <p>eMail</p>
+            <input type="text" name="email" v-model="email">
+            <button v-on:click="createUser">Sign Out</button>
+        </div>
     </div>
-    <router-view />
-  </div>
 </template>
-<script>
 
-export default {
-  name: "App",
-  data() {
-    return {
-      selected: "Toulouse",
-      options: [{ value: "Toulouse" }, { value: "Paris" }, { value: "Nice" }],
-    };
-  },
-};
+<script>
+  import {API} from "./api/api"
+    export default {
+        data() {
+            return{
+                name: null,
+                email: null,
+            }
+        },
+        methods: {
+          createUser() {
+            API.createUser(this.email,this.name)
+          }
+          ,
+          updateUser(){
+            
+          },
+          getUser(){
+            console.log(this.email+" fzefezf "+ this.name)
+            API.getUserByEmailAndUsername(this.email,this.name)
+          },
+          deleteUser() {
+
+          },
+        }
+    }
+    
+
 </script>
 
+<style >
+#container{
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+}
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.container {
-  background-color: #ededed;
-  max-width: 70%;
-  padding: 48px;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 12px;
-}
-.select {
-  background-color: #ffffff;
-  width: 70%;
-  padding: 8px;
-  margin-bottom: 12px;
-  border-color: #aaaaaa;
-  border-radius: 8px;
-}
-.button {
-  background-color: #1e90ff;
-  border: none;
-  color: white;
-  padding: 12px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  border-radius: 8px;
+.sign{
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  background-color: #ededed ;
 }
 </style>
